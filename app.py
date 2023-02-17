080831a2cc0dd09ca3be25875ca45a965fd9aec7
ChiuChienCheng <b910925123@gmail.com>
2023-02-17 12:55:36 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import os
 import requests
-import logging
 from linebot import LineBotApi, WebhookHandler
-from linebot.models import MessageEvent, TextMessage, TextSendMessage, PostbackEvent
-from linebot.exceptions import LineBotApiError
+from linebot.exceptions import InvalidSignatureError, LineBotApiError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
 from flask import Flask, request, abort
 
-app = Flask(__name__)
 
+user_state = {}
 # 商品字典
 product_dict = {
     '30': '531',
@@ -20,7 +20,6 @@ product_dict = {
     '240': '538'
 }
 
-user_state = {}
 
 # 访问 CHANNEL_ACCESS_TOKEN 环境变量
 line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
@@ -31,6 +30,9 @@ handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
 
 # 設定 API key
 API_KEY = os.environ.get("API_KEY")
+
+app = Flask(__name__)
+
 
 @app.route("/line_callback", methods=['POST'])
 def line_callback():
@@ -47,41 +49,33 @@ def line_callback():
 def handle_message(event):
     user_id = event.source.user_id
     message = event.message.text
-    if message == '下單':
+
+    if user_id in user_state and user_state[user_id][0] == 'handle_order_time_response':
+        # 当用户已经输入下单需要的时间，就询问用户商品的直播网址
+        product_id = product_dict.get(message)
+        if product_id:
+            # 如果商品存在，询问商品的直播网址
+            user_state[user_id] = ('handle_link_response', product_id, '', message)
+            ask_link(event, message)
+        else:
+            # 如果商品不存在，提示用户重新输入下单需要的时间
+            reply_message = TextSendMessage(text='輸入的時間不正確，請重新輸入下單需要的時間：30 / 60 / 90 / 120 / 150 / 180 / 210 / 240')
+            line_bot_api.reply_message(event.reply_token, reply_message)
+            return
+
+    if message != '下單':
+        # 如果用户没有发送 "下单"，则直接回复提示消息
+        reply_message = TextSendMessage(text='抱歉，我不了解您的意思，請輸入「下單」來開始下單流程。')
+        line_bot_api.reply_message(event.reply_token, reply_message)
+        # 設定使用者的狀態為「處理下單」
+        user_state[event.source.user_id] = 'handle_order_time_response'
+        # 詢問使用者下單需要的時間
+        ask_order_time(event)
+    else:
         # 啟動下單流程
         state = ask_order_time(event)
         # 設定使用者的狀態為「處理下單」
         user_state[user_id] = state
-    else:
-        if user_id in user_state and user_state[user_id] == 'handle_order_time_response':
-            # 當使用者已經輸入下單需要的時間，就詢問使用者商品的直播網址
-            product_id = product_dict.get(message)
-            if product_id:
-                # 如果商品存在，詢問商品的直播網址
-                user_state[user_id] = ('handle_link_response', product_id, '', message)
-                ask_link(event, message)
-            else:
-                # 如果商品不存在，提示使用者重新輸入下單需要的時間
-                reply_message = TextSendMessage(text='輸入的時間不正確，請重新輸入下單需要的時間：30 / 60 / 90 / 120 / 150 / 180 / 210 / 240')
-                line_bot_api.reply_message(event.reply_token, reply_message)
-        elif user_id in user_state and user_state[user_id][0] == 'handle_link_response':
-            # 當使用者已經輸入商品的直播網址，就詢問使用者商品數量
-            state = handle_link_response(event, user_state[user_id][1], '', user_state[user_id][3], message)
-
-            user_state[user_id] = state
-        elif user_id in user_state and user_state[user_id][0] == 'handle_order_quantity':
-            # 當使用者已經輸入商品數量，就進行下單
-            handle_order_quantity(event, *user_state[user_id][1:])
-            # 下單完成後，清空使用者狀態
-            del user_state[user_id]
-        else:
-            # 無法辨識的訊息
-            reply_message = TextSendMessage(text='抱歉，我不了解您的意思，請輸入「下單」來開始下單流程。')
-            line_bot_api.reply_message(event.reply_token, reply_message)
-            # 設定使用者的狀態為「處理下單」
-            user_state[event.source.user_id] = 'handle_order_time_response'
-            # 詢問使用者下單需要的時間
-            ask_order_time(event)
 def ask_order_time(event):
     reply_message = TextSendMessage(text='請輸入直播時長(分鐘)：\n30 / 60 / 90 / 120 / 150 / 180 / 210 / 240')
     line_bot_api.reply_message(event.reply_token, reply_message)
@@ -105,29 +99,60 @@ def ask_quantity(event, product_name):
 def handle_order_quantity(event, product_id, link, product_name):
     # 取得使用者輸入的商品數量
     quantity = event.message.text
-    # 向 API 發送下單的請求
-    url = 'https://fbliveviewerbot.com/api/v2'
-    payload = {'key': API_KEY, 'action': 'add', 'service': product_id, 'quantity': quantity, 'link': link}
-    response = requests.post(url, data=payload)
-    # 解析 API 回傳的 JSON 資料
-    data = response.json()
-    if 'order' in data:
-        order_id = data['order']
-        # 向 API 發送查詢餘額的請求
+    # 確認資料是否正確
+    user_id = event.source.user_id
+    user_state[user_id] = ('confirm_order', product_id, link, product_name, quantity)
+    confirm_order(event, product_id, link, product_name, quantity)
+
+def confirm_order(event, product_id, link, product_name, quantity):
+    # 確認使用者資料是否正確
+    message = f'請確認您所要下單的資料：\n\n商品：{product_name}\n網址：{link}\n數量：{quantity}\n\n輸入 "確認" 確認訂單，輸入 "取消" 取消訂單。'
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    user_id = event.source.user_id
+    message = event.message.text
+
+if user_id in user_state and user_state[user_id][0] == 'confirm_order':
+    # 如果正在确认订单信息，根据用户的回复执行相应的操作
+    if message == '是':
+        # 用户确认下单信息，向 API 发送下单请求
+        product_id, link, product_name, quantity = user_state[user_id][1:]
         url = 'https://fbliveviewerbot.com/api/v2'
-        payload = {'key': API_KEY, 'action': 'balance'}
+        payload = {'key': API_KEY, 'action': 'add', 'service': product_id, 'quantity': quantity, 'link': link}
         response = requests.post(url, data=payload)
         # 解析 API 回傳的 JSON 資料
         data = response.json()
-        if 'balance' in data:
-            balance = data['balance']
-            currency = data['currency']
-            reply_message = f'已下單成功，訂單編號為 {order_id}，您的餘額為 {balance} {currency}'
+        if 'order' in data:
+            order_id = data['order']
+            # 向 API 發送查詢餘額的請求
+            url = 'https://fbliveviewerbot.com/api/v2'
+            payload = {'key': API_KEY, 'action': 'balance'}
+            response = requests.post(url, data=payload)
+            # 解析 API 回傳的 JSON 資料
+            data = response.json()
+            if 'balance' in data:
+                balance = data['balance']
+                currency = data['currency']
+                reply_message = f'已下單成功，訂單編號為 {order_id}，您的餘額為 {balance} {currency}'
+            else:
+                reply_message = f'已下單成功，訂單編號為 {order_id}，但無法取得餘額資訊'
         else:
-            reply_message = f'已下單成功，訂單編號為 {order_id}，但無法取得餘額資訊'
-    else:
-        reply_message = '下單失敗，請稍後再試'
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
+            reply_message = '下單失敗，請稍後再試'
+        user_state[user_id] = None
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
+    elif message == '否':
+        # 用户取消下单，重置用户状态
+        user_state[user_id] = None
+        reply_message = TextSendMessage(text='已取消下單。')
+        line_bot_api.reply_message(event.reply_token, reply_message)
+
+# 重置用户状态
+user_state[user_id] = None
+# 回复用户下单结果
+line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_message))
+
 # 設定 Line bot 的 Webhook URL，讓 Line 可以將使用者訊息傳送到這個網址
 @app.route("/callback", methods=['POST'])
 def callback():
